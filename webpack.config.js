281728a1c7dfaeaf9ca3a839bcaa5df4fa930992
Orion Sasson <adikadosh@Yogevs-MacBook-Air.local>
2018-02-07 23:11:09 +0200

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,51 @@
+// const path = require('path');
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+// const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+// module.exports = {
+//   entry: './src/index.js',
+//   output: {
+//     filename: 'static/js/app.bundle.js',
+//     path: path.resolve(__dirname, 'dist')
+//   },
+//   module: {
+//     rules: [
+//       { test:/\.js$/, use: 'babel-loader' , exclude: /node_modules/ },
+//      { test:/\.scss$/, use: ['style-loader','css-loader','sass-loader'] },
+//       {
+//         test: /\.css$/,
+//         use: ExtractTextPlugin.extract({
+//           fallback: "style-loader",
+//           use: "css-loader"
+//         })
+//       },
+//       {
+//         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+//         loader: 'url-loader',
+//         options: {
+//           limit: 10000
+//         }
+//       },
+//     ]
+//   },
+//   plugins: [
+//     new ExtractTextPlugin("static/css/globals.css"),
+//     new CopyWebpackPlugin([
+//           { from: 'src/index.html' },
+//           { from: 'src/static/data'  , to: 'static/data'},
+//           // { from: 'src/static/images', to: 'static/images'},
+//       ])
+//   ],
+//   devServer: {
+//     contentBase: path.join(__dirname, "dist"),
+//     compress: true,
+//     port: 3000,
+//     stats:"minimal",
+//     open:true
+//   },
+//   devtool:"source-map"
+// };
+
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
@@ -11,7 +59,8 @@ module.exports = {
   module: {
     rules: [
       { test:/\.js$/, use: 'babel-loader' , exclude: /node_modules/ },
-     { test:/\.scss$/, use: ['style-loader','css-loader','sass-loader'] },
+      { test:/\.scss$/, use: ['style-loader','css-loader','sass-loader'] },
+     // { test:/\.css$/, use: ['style-loader','css-loader'] },
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
@@ -33,7 +82,6 @@ module.exports = {
     new CopyWebpackPlugin([
           { from: 'src/index.html' },
           { from: 'src/static/data'  , to: 'static/data'},
-          // { from: 'src/static/images', to: 'static/images'},
       ])
   ],
   devServer: {
